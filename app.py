ceab758ebea2561ffe78f2f3cd60ad52bed9cbeb
lawan0988 <89177152+lawan0988@users.noreply.github.com>
2021-12-15 14:39:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -158,7 +158,13 @@ def event_handle(event,json_line):
         elif msg == "ไปเที่ยวมั้ย":
             replyObj = TextSendMessage(text="ไปที่ไหนกันดีล่ะ")
             line_bot_api.reply_message(rtoken, replyObj)
-        else : 
+        else msg == "covid" :
+            url = "https://covid19.ddc.moph.go.th/api/Cases/today-cases-all"
+            response = requests.get(url)
+            response = response.json()
+            replyObj = TextSendMessage(text=str(response))
+            line_bot_api.reply_message(rtoken, replyObj)
+        else :
             headers = request.headers
             json_headers = ({k:v for k, v in headers.items()})
             json_headers.update({'Host':'bots.dialogflow.com'})
